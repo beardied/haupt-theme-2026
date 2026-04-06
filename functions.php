272d840fec2edde8c60260db84c1874410b23fb8
Haupt Dev <dev@hauptrecruitment.co.uk>
2026-04-06 23:05:44 +0100

    Version bump to 1.0.23 for cache busting

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Theme version for cache busting
-define('HAUPT_VERSION', '1.0.22');
+define('HAUPT_VERSION', '1.0.23');
 define('HAUPT_DIR', get_template_directory());
 define('HAUPT_URI', get_template_directory_uri());
 
